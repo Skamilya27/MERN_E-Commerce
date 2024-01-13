5c3f78a5873b5c9063bbcb7b85721faa8c7f6476
Suman <sumankamilya27@gmail.com>
2024-01-13 20:15:30 +0530

    solved checked unchecked issue

--- src/features/product-list/productAPI.js
+++ src/features/product-list/productAPI.js
@@ -1,23 +1,29 @@
 export function fetchAllProducts() {
-    return new Promise(async (resolve) =>{
-      const response = await fetch('http://localhost:8080/products') 
-      const data = await response.json()
-      resolve({data})
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter, sort) {
+  let queryString = "";
+  for (let key in filter) {
+    const categoryValues = filter[key];
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
     }
-    );
   }
-  
-  export function fetchProductsByFilters(filter) {
-    let queryString = Object.keys(filter)
-    .map(key => `${key}=${filter[key]}`)
-    .join('&');
-    console.log(queryString);
-  
-    return new Promise(async (resolve) =>{
-      const response = await fetch('http://localhost:8080/products?'+queryString) 
-      const data = await response.json()
-      resolve({data})
-    }
-    );
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
   }
-  
+
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    resolve({ data });
+  });
+}
